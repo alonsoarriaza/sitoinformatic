ae79d721ddf780f8bf481cffa62bda9354051696
Alonso Feria Arriaza <alonsoarriaza03@gmail.com>
2026-02-03 10:41:01 +0100

    Componentes: Corregidas importaciones de Page y Pageable en Service y Repository

--- sitoinformatic/src/main/java/com/shop/proyect/sitoinformatic/repository/ComponentRepository.java
+++ sitoinformatic/src/main/java/com/shop/proyect/sitoinformatic/repository/ComponentRepository.java
@@ -1,14 +1,16 @@
 package com.shop.proyect.sitoinformatic.repository;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
-
 import com.shop.proyect.sitoinformatic.model.Component;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 
-public interface ComponentRepository extends JpaRepository <Component,Long> {
-
+public interface ComponentRepository extends JpaRepository<Component, Long> {
     
+    // Busca componentes por categoría ignorando mayúsculas/minúsculas
+    Page<Component> findByCategoryIgnoreCase(String category, Pageable pageable);
 
+    // Busca componentes cuyo nombre contenga el texto (para el buscador)
+    Page<Component> findByProductNameContainingIgnoreCase(String name, Pageable pageable);
 }
